4c0db7696d6f2372a813e2ef0d78d14297928558
Krister <krister.torsvik@gmail.com>
2016-08-04 13:24:26 +0200

    project done

--- index.js
+++ index.js
@@ -1,58 +1,66 @@
-
-var myInterval = setInterval(myRepeatingInterval, interval);
 var merp = document.getElementById("hello")
+var derp = document.getElementById("container")
 var length = 1;
 var height = 1;
 var interval = 10;
 var slider = document.getElementById("slider");
 setInterval(function() {
-  interval = slider.value;}, 10)
+  interval = slider.value;}, 10);
 
 function myRepeatingInterval() {
-  clearInterval(myInterval);
-	length = length + 1;
+  merp.style.borderRadius = "5px";
+  merp.style.transition = "all 1s ease-out";
+  length = length + 1;
 	height = height + 1;
-	if (length < 50) {
+	if (length <= 50) {
+    derp.style.backgroundColor = "blue"
+    merp.style.backgroundColor = "red";
 		merp.style.width = length + "px"
-		//console.log(length);
 	} else if (length >= 50 && length <= 100) {
 		merp.style.backgroundColor= "blue";
-		merp.style.top = "220px"
+		merp.style.top = "240px"
 		merp.style.left = "75px"
 		merp.style.width = length + "px"
+    derp.style.backgroundColor = "black"
 	} else {
 		merp.style.top = "100px"
 		merp.style.left = "0"
 		length = 1;
 		height = 12;
-	}
+	};
 
 	if (height < 50) {
 		merp.style.height = height + "px"
 		merp.style.backgroundColor = "green"
-		//console.log(height);
+    derp.style.backgroundColor = "yellow"
 	} else if (height >= 50 && height <= 100) {
+    derp.style.backgroundColor = "red"
 		merp.style.left = "100px"
 		merp.style.top = "0px"
 		merp.style.height = height + "px"
 	} else {
-		merp.style.left = "200px"
+		merp.style.left = "220px"
 		merp.style.top = "150px"
-		merp.style.backgroundColor= "red";
 		height = 1;
 		length = 25;
-	}
-}
-
+	};
+};
+var myInterval;
 
  function runThis() {
-   var myInterval = setInterval(myRepeatingInterval, interval);
- }
+    if (!myInterval) {
+      myInterval = setInterval(myRepeatingInterval, interval);
+    }
+ };
+
   function reset() {
-    clearInterval(myInterval);
-    interval = null;
-     length = 1;
-     height = 1;
+     merp.style.height = 1;
+     merp.style.width = 1;
      merp.style.top = "0";
      merp.style.left = "0";
-  }
+  };
+
+  function stop() {
+    clearInterval(myInterval);
+    myInterval = null;
+  };
